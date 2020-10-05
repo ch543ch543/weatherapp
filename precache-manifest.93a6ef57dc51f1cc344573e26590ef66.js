46a007305cbabc0f88243d75f4831b7ae6fae23f
Yichun Tsai <ch543ch543@gmail.com>
2020-10-05 13:54:22 +0100

    Updates

---
+++ precache-manifest.93a6ef57dc51f1cc344573e26590ef66.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9835bee5131cb71b38d439ca55f77d97",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "efcc428ac6a8e3709716",
+    "url": "/weatherapp/static/css/main.8e429e7f.chunk.css"
+  },
+  {
+    "revision": "bb14f01ca49de6a68074",
+    "url": "/weatherapp/static/js/2.a00ae6d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.a00ae6d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efcc428ac6a8e3709716",
+    "url": "/weatherapp/static/js/main.1da7b759.chunk.js"
+  },
+  {
+    "revision": "8862584b26a768bb4b89",
+    "url": "/weatherapp/static/js/runtime-main.cb4f0870.js"
+  },
+  {
+    "revision": "032bf6d985eb0dfb558fddacf39006c7",
+    "url": "/weatherapp/static/media/Spinner.032bf6d9.gif"
+  },
+  {
+    "revision": "611645789efc3a4dafdd71add51df2c5",
+    "url": "/weatherapp/static/media/cloudy.61164578.svg"
+  },
+  {
+    "revision": "5ff3dbf1f3906048c30ce4be76f21844",
+    "url": "/weatherapp/static/media/rain.5ff3dbf1.svg"
+  },
+  {
+    "revision": "5fa9d007fd61254260ef4ec0f92445ff",
+    "url": "/weatherapp/static/media/snowing.5fa9d007.svg"
+  },
+  {
+    "revision": "36ef11ddd2c0a9b5a683da2468f52110",
+    "url": "/weatherapp/static/media/sun.36ef11dd.svg"
+  },
+  {
+    "revision": "c25afb20c27fe9e03d336cdccece1059",
+    "url": "/weatherapp/static/media/thermometer.c25afb20.svg"
+  }
+]);
